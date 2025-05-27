a8d5c070dc86c248c16ad98e31a0910d00f9cd2c
Henrique Diego <henriquedsabara@gmail.com>
2025-05-27 13:37:45 -0300

    Atualiza o código principal e o ajuste do READ.ME

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Descrição
 
-Este projeto foi desenvolvido para a disciplina de Robótica da faculdade, utilizando o **Lego EV3 Mindstorm**. O objetivo do projeto é criar uma garra robótica que executa movimentos de acordo com a cor detectada por um sensor de cor. A garra pode abrir e fechar, e a movimentação é controlada por três eixos.
+Este projeto foi desenvolvido para a disciplina de Robótica da faculdade Impacta Tecnologia, utilizando o **Lego EV3 Mindstorm**. O objetivo do projeto é criar uma garra robótica que executa 3 movimentos de acordo com uma das 3 cores detectadas(Vermelho, Verde e Amarelo) por um sensor de cor. A garra pode abrir e fechar, e a movimentação é controlada por três eixos.
 
 O código implementado no EV3Brick permite que o robô execute diferentes sequências de movimentos baseadas na cor do objeto detectado pelo sensor. As cores que são reconhecidas são **vermelho**, **azul** e **verde**, e para cada cor há uma sequência de movimentos específica.
 
@@ -11,14 +11,13 @@ O código implementado no EV3Brick permite que o robô execute diferentes sequê
 * Lego EV3 Mindstorm
 * Pybricks MicroPython
 * Sensor de Cor (Porta S1)
-* Motores para movimentação da garra e dos eixos X, Y e Z
+* Motores para movimentação da garra e dos eixos X e Y
 
 ## Estrutura do Projeto
 
 * **Motor A:** Controla o motor da garra
-* **Motor B:** Controla o eixo Z
+* **Motor B:** Controla o eixo Y
 * **Motor C:** Controla o eixo X
-* **Motor D:** Controla o eixo Y
 * **Sensor S1:** Detecta a cor do objeto
 
 ## Como Funciona
@@ -33,11 +32,12 @@ O código implementa a lógica para abrir/fechar a garra e mover os eixos com ba
 * **eixo\_x\_right()**: Move o eixo X para a direita.
 * **eixo\_x\_left()**: Move o eixo X para a esquerda.
 * **acao\_baseada\_na\_cor(cor)**: Executa uma sequência de movimentos com base na cor detectada.
+* **sobe\_garra\_amarelo()**: Sobe o eixo y quando dectectado a cor amarela
 
 ### Sequência de Ações
 
-* **Cor Vermelha**: Abre e fecha a garra, move o eixo Y para cima e para baixo, e o eixo X para a direita e esquerda.
-* **Cor Azul**: Movimenta o eixo X para a direita e para a esquerda.
+* **Cor Vermelha**: Sobe a garra, eixo x vai para a direita, eixo y desce, fecha a garra, sobe o eixo y, eixo x vai para esquerda(volta ao centro), abre a garra.
+* **Cor Amarelo**: Movimenta o eixo X para a direita e para a esquerda.
 * **Cor Verde**: Move o eixo Y para cima e para baixo.
 
 ## Como Rodar o Código
@@ -49,9 +49,15 @@ O código implementa a lógica para abrir/fechar a garra e mover os eixos com ba
    * Eixo X na porta C
    * Eixo Y na porta D
    * Sensor de cor na porta S1
-3. **Carregue o código no seu EV3** e execute.
-4. **Observe a detecção das cores** e a execução da sequência de movimentos com base na cor detectada.
+
+3. **Deixe a garra aberta** antes de rodar o código
+4. **Carregue o código no seu EV3** e execute.
+5. **Observe a detecção das cores** e a execução da sequência de movimentos com base na cor detectada.
 
 ## Grupo/Colaboradores
+
+Lista:
+Henrique Diego
+
 
 Se você quiser contribuir com o projeto, fique à vontade para criar um *fork*, fazer alterações e enviar um *pull request*.
--- main.py
+++ main.py
@@ -10,30 +10,36 @@ import time
 # Inicialização dos dispositivos
 ev3 = EV3Brick()
 motor_garra = Motor(Port.A)
+motor_y = Motor(Port.B)
 motor_x = Motor(Port.C)
-motor_y = Motor(Port.D)
-motor_z = Motor(Port.B)
 sensor_cor = ColorSensor(Port.S1)
 
+#ATENÇÃO: SEMPRE DEIXAR A GARRA ABERTA PARA A LÓGICA DO CÓDIGO
+def sobe_garra_amarelo():
+    motor_y.run_angle(300, -550, then=Stop.HOLD, wait=True)
 # Função para abrir ou fechar a garra
 def acao_garra(abrir=True):
     if abrir:
-        motor_garra.run_time(100, 2000, then=Stop.HOLD, wait=True)  # Abre a garra
+        motor_garra.run_time(100, 1000, then=Stop.HOLD, wait=True)  # Abre a garra
+        # motor_garra.run_angle(300, -250, then=Stop.HOLD, wait=True)
     else:
-        motor_garra.run_time(-100, 2000, then=Stop.HOLD, wait=True)  # Fecha a garra
+        motor_garra.run_time(-100, 1000, then=Stop.HOLD, wait=True)  # Fecha a garra
+        # motor_garra.run_angle(300, -250, then=Stop.HOLD, wait=True)
 
 # Funções para movimentação dos eixos
 def eixo_y_cima():
-    motor_y.run_angle(300, 360, then=Stop.HOLD, wait=True)
+    motor_y.run_angle(300, -250, then=Stop.HOLD, wait=True)
+    # motor_y.run_time(300, 3000, then=Stop.HOLD, wait=True)
 
 def eixo_y_baixo():
-    motor_y.run_angle(300, -180, then=Stop.HOLD, wait=True)
+    motor_y.run_angle(300, 500, then=Stop.HOLD, wait=True)
+    # motor_y.run_time(300, 3000, then=Stop.HOLD, wait=True)
 
 def eixo_x_right():
-    motor_x.run_angle(300, 360, then=Stop.HOLD, wait=True)
+    motor_x.run_angle(300, 320, then=Stop.HOLD, wait=True)
 
 def eixo_x_left():
-    motor_x.run_angle(300, -360, then=Stop.HOLD, wait=True)
+    motor_x.run_angle(300, -320, then=Stop.HOLD, wait=True)
 
 # Função para executar uma sequência de ações com base na cor detectada
 def acao_baseada_na_cor(cor):
@@ -41,26 +47,46 @@ def acao_baseada_na_cor(cor):
         # Exemplo de sequência para a cor vermelha
         print("Cor vermelha detectada - Executando sequência")
         eixo_y_cima()
-        acao_garra(abrir=True)
-        acao_garra(abrir=False)
         eixo_x_right()
+        eixo_y_baixo()
+        acao_garra(abrir=True)  # Fecha a garra
+        eixo_y_cima()
         eixo_x_left()
+        wait(2000)
+        acao_garra(abrir=False)  # Abre para soltar
+        wait(2000)  
+        print(motor_x.angle(), motor_y.angle())
+        motor_y.run_target(300, 0, then=Stop.HOLD, wait=True)
+        motor_x.run_target(300, 0, then=Stop.HOLD, wait=True)
+
+    elif cor == Color.YELLOW:
+        # Exemplo de sequência para a cor azul
+        print("Cor amarela detectada - Executando sequência")
         eixo_y_baixo()
         acao_garra(abrir=True)
-    elif cor == Color.BLUE:
-        # Exemplo de sequência para a cor azul
-        print("Cor azul detectada - Executando sequência")
-        eixo_x_right()
-        wait(1000)
-        eixo_x_left()
-        wait(1000)
+        wait(2000)
+        sobe_garra_amarelo()
+        wait(2000)
+        acao_garra(abrir=False)
+        print(motor_x.angle(), motor_y.angle())
+        motor_y.run_target(300, 0, then=Stop.HOLD, wait=True)
+        motor_x.run_target(300, 0, then=Stop.HOLD, wait=True)
     elif cor == Color.GREEN:
         # Exemplo de sequência para a cor verde
         print("Cor verde detectada - Executando sequência")
         eixo_y_cima()
-        wait(1000)
+        eixo_x_left()
         eixo_y_baixo()
-        wait(1000)
+        acao_garra(abrir=False)
+        eixo_y_cima()
+        eixo_x_right()
+        wait(2000)
+        acao_garra(abrir=True)
+        wait(2000)
+        print(motor_x.angle(), motor_y.angle())
+        motor_y.run_target(300, 0, then=Stop.HOLD, wait=True)
+        motor_x.run_target(300, 0, then=Stop.HOLD, wait=True)
+
 
 # Loop principal
 while True:
@@ -73,4 +99,4 @@ while True:
     print("Cor detectada:", cor_detectada)
     acao_baseada_na_cor(cor_detectada)
     
-    wait(5000)  # Evita leituras excessivas
+    wait(2000)  # Evita leituras excessivas
